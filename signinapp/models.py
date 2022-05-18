e15f33319cb2fb440ad3c98028c40b176055b0c9
Sagar <sagarkaushik1998@gmail.com>
2022-05-18 09:41:36 +0530

    Created Model in Models.py in The App

--- signinapp/models.py
+++ signinapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class SignDB(models.Model):
+    UserNameF=models.CharField(max_length=122)
+    EmailF=models.EmailField(blank=True)
+    PasswordF=models.CharField(max_length=122)
+    
