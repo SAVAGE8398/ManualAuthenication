1d7f8f287d705cf6f42dfc372d56349f8d67f74c
Sagar <sagarkaushik1998@gmail.com>
2022-05-18 12:05:55 +0530

    Form Submitted With Email And Password In DATABASE

--- signinapp/views.py
+++ signinapp/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from signinapp.models import SignDB
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 def signup(request):
+    if request.method == 'POST':
+        varEmail = request.POST.get('emailhtml')
+        varPassword = request.POST.get('passwordhtml')
+        varSubmit=SignDB(PasswordF=varPassword,EmailF=varEmail)
+        varSubmit.save()
+        return redirect('signin')
     return render(request, 'signup.html')
+def signin(request):
+    return render(request, 'signin.html')
     
